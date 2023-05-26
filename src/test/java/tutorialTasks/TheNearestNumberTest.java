d87b0fb1c928dff327c817199e12c0dba510c74c
Elnur <ekasimov2012@gmail.com>
2023-05-26 11:37:31 +0300

    created tests for main in TheNearestNumber

--- src/test/java/tutorialTasks/TheNearestNumberTest.java
+++ src/test/java/tutorialTasks/TheNearestNumberTest.java
@@ -6,4 +6,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TheNearestNumberTest {
 
+    @Test
+    void testThatMethodWorksCorrectlyWhenListIsEmpty () {
+        //given
+        //when
+        //then
+
+    }
+
+    @Test
+    void testThatMethodWorksCorrectlyWithPositiveNumbersOnly () {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void testThatMethodWorksCorrectlyWithNegativeNumbersOnly () {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void testThatMethodWorksCorrectlyWhenEqualsNegativeAndPositiveNumbers () {
+        //given
+        //when
+        //then
+    }
+    @Test
+    void testThatMethodWorksCorrectlyWhenEqualsTwoNegativeNumbers () {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void testThatMethodWorksCorrectlyWhenEqualsTwoPositiveNumbers () {
+        //given
+        //when
+        //then
+    }
 }
